670b6d392bdf1a97c0350d96ac855328c0445011
Eliel Lima <elieldalbo@gmail.com>
2020-10-19 21:31:00 -0300

    Updates

---
+++ precache-manifest.6a33d9b591a87db02a31b3ca3d88ba5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae7bb1286d25b5365a9b7a22dea7d2c",
+    "url": "/deputados/index.html"
+  },
+  {
+    "revision": "fdb79668e0cc30a85209",
+    "url": "/deputados/static/css/main.e9cff57e.chunk.css"
+  },
+  {
+    "revision": "19feb3586691c488f573",
+    "url": "/deputados/static/js/2.8d7786d5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/deputados/static/js/2.8d7786d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb79668e0cc30a85209",
+    "url": "/deputados/static/js/main.4745ac0f.chunk.js"
+  },
+  {
+    "revision": "934e15dd504b2735a61c",
+    "url": "/deputados/static/js/runtime-main.c64e1c6a.js"
+  }
+]);
